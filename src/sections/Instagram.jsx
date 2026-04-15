62b380f8923eebad30973f943f85b4ae351c6c3e
lizb <loo4love90@gmail.com>
2026-04-15 21:25:19 +0900

    feat 전체적인 내용 수정

--- src/sections/Instagram.jsx
+++ src/sections/Instagram.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles/Instagram.scss";
-
+import instar from '../util/instar'
 const Instagram = () => {
   
   const feedList = [1, 2, 3, 4, 5, 6];
@@ -14,13 +14,16 @@ const Instagram = () => {
       </div>
 
       <ul className="instar-list">
-        {feedList.map((num) => (
-          <li key={num}>
-            <a
-              href="#"
-              style={{ backgroundImage: `url(/img/instar${num}.jpg)` }}
-            ></a>
-          </li>
+        {instar.map((i)=>(
+
+        <li key={i.id}>
+          <a href={i.link}
+          alt={i.alt}
+          style={{backgroundImage:`url(${i.image})`}}
+
+          // 배경 넣어보기
+          >{i.id}</a>
+        </li>
         ))}
       </ul>
     </div>
